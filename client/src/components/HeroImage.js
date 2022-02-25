f8c45e9bf4555b3c928f594848060e91901135c1
HobbZ <zachobba@gmail.com>
2022-02-25 22:40:36 +1000

    svg truck now added

--- client/src/components/HeroImage.js
+++ client/src/components/HeroImage.js
@@ -74,38 +74,30 @@ function objectToGen() {
       <>
       <svg width = {genBookWidth} height = {genBookHeight}>
       
+      <g>
         {/*book*/}
       <rect x="0" y= "0" width="100%" height= "100%" fill={chosenColour} />
       
-      
-      <g>
       {/*spine*/}
       <rect x="0" y="0" width={spineWidth} height="100%" fill={dayChange()} />
-      </g>
 
-      <g>
       {/*light spine*/}
       <rect x={0 + (spineWidth/8)} y="0" width={spineWidth/4} height="100%" fill={chosenColour} />
-      </g>
 
-      <g>
       {/*pages*/}
       <rect x={0 + (spineWidth/3)} y="0" width={spineWidth/2} height="100%" fill="white" style={{stroke:"black", strokeWidth:"1"}}/>
-      </g>
 
-      <g>
       {/*cover*/}
       <rect x={0 + (spineWidth + spineWidth/4)} y={0 + (genCoverHeight/4)} width={genCoverWidth} height={genCoverHeight} fill={createRandomColor()} />
-      </g>
 
       {/*shade overlay*/}
       <rect x="0" y= "0" width="100%" height= "100%" fill={dayChange()} />
+      </g>
 
       </svg>
-      
       </>
     )
-    }
+    };
 
     if (object[randNumber] === "collectable") {
       randBookWidth()
@@ -119,69 +111,163 @@ function objectToGen() {
 
       let spineWidth = genBookWidth/4;
 
-      console.log("book ", genBookWidth, genBookHeight, spineWidth)
+      console.log("camera ", genBookWidth, genBookHeight, spineWidth)
         
     return (
       <>
       <svg width = {genBookWidth+spineWidth} height = {genBookHeight/2}>
       
+      <g>
         {/*old camera*/}
       <rect x="0" y= "0" width="100%" height= "100%" fill={chosenColour} />
-      
-      <g>
+
       {/*stripe*/}
       <rect x={0 + (spineWidth/3)} y="0" width={randBookWidth()[0]/10} height="100%" fill={createRandomColor()}/>
-      </g>
 
-      <g>
       {/*strap*/}
       <rect x={0 - (spineWidth/2)} y="50%" width={spineWidth} height="10%" fill={createRandomColor()}/>
-      </g>
 
-      <g>
       {/*side*/}
       <rect x="0" y="0" width={spineWidth} height="100%" fill={dayChange()} />
-      </g>
 
-      <svg x={0 + (spineWidth + spineWidth/4)} y={0 + (genCoverHeight/4)} width={genCoverWidth/4} height={genCoverHeight/4}>
- 
-      <g>
       {/*viewfinder*/}
-      <rect x="0" y="0" width="100%" height="100%" fill={cameraLense}/>
-      </g>
-      </svg>
+      <rect x={0 + (spineWidth + spineWidth/4)} y={0 + (genCoverHeight/4)} width={genCoverWidth/4} height={genCoverHeight/4} fill={cameraLense}/>
 
+
+      {/*parts for lense*/}
       <svg width = {genBookWidth} height = {genBookHeight/2} x ={spineWidth-10} y ="5%">
       <g>
       {/*camera surround*/}
       <circle cx= "45%" cy="50%" r={(genBookWidth/2)/3} fill="black" />
-      </g>
 
-      <g>
       {/*lense 1*/}
       <circle cx="49%" cy="50%" r={(genBookWidth/2)/3} fill={createRandomColor()} />
-      </g>
 
-      <g>
       {/*lense 2*/}
       <circle cx="50%" cy="50%" r={(genBookWidth/2)/4} fill="black" />
-      </g>
 
-      <g>
       {/*lense 3*/}
       <circle cx="50%" cy="50%" r={(genBookWidth/2)/6} fill={cameraLense} />
       </g>
       </svg>
+
       {/*shade overlay*/}
       <rect x="0" y= "0" width="100%" height= "100%" fill={dayChange()} />
+      </g>
       </svg>
-      
       </>
     )
     }
 
     if (object[randNumber] === "toy") {
 
+      randBookWidth()
+      let genBookWidth = randBookWidth()[0]
+      let genBookHeight = randBookWidth()[1]
+
+      let windScreen = createRandomColor();
+
+      let spineWidth = genBookWidth/4;
+
+      let tireSize = genBookHeight/6;
+
+      let wheelColour= createRandomColor();
+
+      console.log("truck ", genBookWidth, genBookHeight, spineWidth)
+
+      return (
+        <>
+        {/*truck toy*/}
+      <svg width = {genBookWidth+spineWidth} height = {genBookHeight/2} x="0" y="0">
+      
+      {/*truck bed collection*/}
+      <svg width = "65%" height = "100%" x="15%" y="0%" >
+
+      {/*truck bed*/}
+      <svg width = "100%" height = "80%" x="0%" y="0%" >
+      <g>
+      <rect x="0" y= "0" width="100%" height= "100%" fill={createRandomColor()}/>
+
+      {/*front of bed*/}
+      <rect x="0" y="0" width={spineWidth+10} height="100%" fill={dayChange()} />
+      </g>
+      </svg>
+
+        {/*tire 1*/}
+        <svg width = {tireSize} height = {tireSize} x ="65%" y ="60%">
+                <g>
+                {/*tire sidewell*/}
+                <circle cx="48%" cy="50%" r={(tireSize)/2} fill="black" />
+          
+                {/*rim*/}
+                <circle cx="49%" cy="50%" r={(tireSize)/3} fill={wheelColour} />
+
+                {/*inner rim*/}
+                <circle cx="49%" cy="50%" r={(tireSize)/4} fill="black" />
+
+                {/*hubcap*/}
+                <circle cx="49%" cy="50%" r={(tireSize)/5} fill="grey" />
+            
+                {/*outer guard*/}
+                <rect x="0%" y="0%" width= "100%" height="20%" fill={chosenColour} />
+                </g>
+            </svg>
+
+      {/*shade overlay*/}
+      <rect x="0" y= "0" width="100%" height= "80%" fill={dayChange()} />
+      </svg>
+
+
+      {/*cab parts collection*/}
+      <svg width = "36%" height = "100%" x="0" y="0%">
+
+      {/*cab*/}
+      <svg width= "100%" height="80%" x ="0" y="10%">
+      
+      <g>
+      <rect x="0" y= "0%" width="100%" height= "100%" fill={chosenColour} />
+            
+      {/*radiator*/}
+      <rect x={0 + (spineWidth/3)} y="50%" width={randBookWidth()[0]/10} height="40%" fill={createRandomColor()}/>
+
+      {/*windscreen*/}
+      <rect x="0" y="20%" width={spineWidth} height="20%" fill={windScreen}/>
+
+      {/*side Window*/}
+      <rect x={0 + spineWidth} y="20%" width="30%" height="20%" fill={windScreen}/>
+
+      {/*front*/}
+      <rect x="0" y="0%" width={spineWidth} height="100%" fill={dayChange()} />
+      </g>
+      </svg>
+
+          {/*tire 1*/}
+          <svg width = "50%" height = {tireSize} x ="60%" y ="60%">
+                <g>
+                {/*tire sidewell*/}
+                <circle cx="48%" cy="50%" r={(tireSize)/2} fill="black" />
+          
+                {/*rim*/}
+                <circle cx="49%" cy="50%" r={(tireSize)/3} fill={wheelColour} />
+
+                {/*inner rim*/}
+                <circle cx="49%" cy="50%" r={(tireSize)/4} fill="black" />
+
+                {/*hubcap*/}
+                <circle cx="49%" cy="50%" r={(tireSize)/5} fill="grey" />
+            
+                {/*outer guard*/}
+                <rect x="0%" y="0%" width= "100%" height="20%" fill={chosenColour} />
+                </g>
+            </svg>
+                
+          {/*shade overlay*/}
+          <rect x="0" y= "10%" width="100%" height= "80%" fill={dayChange()} />
+      </svg>
+
+</svg>
+</>
+      )
     }
 
 }
